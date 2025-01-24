aa9c5641a6b5644de523522203a13d6ee7c642b0
axelquartz <axelarep@gmail.com>
2025-01-24 11:56:02 -0600

    Display popular movies fetching the API

--- src/main.js
+++ src/main.js
@@ -1,10 +1,23 @@
 async function getPopularMoviesPreview() {
-    const response = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=759afe2f973e62f0426265a22a2fa385&language=es-ES&page=1`);
+    const response = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=es-ES`);
     const data = await response.json();
+    const movies = data.results;
+    movies.forEach(movie => {
+        const trendingPreviewContainer = document.querySelector('#trendingPreview .trendingPreview-movieList');
 
-    console.log(data);
+        const movieContainer = document.createElement('div');
+        movieContainer.classList.add('movie-container');
+
+        const movieImg = document.createElement('img');
+        movieImg.classList.add('movie-img');
+        movieImg.setAttribute('alt', movie.title);
+        movieImg.setAttribute('src', 'https://image.tmdb.org/t/p/w300' + movie.poster_path);
+        movieContainer.append(movieImg);
+        
+        trendingPreviewContainer.append(movieContainer);
+    })
+    console.log(movies);
     
-    return data.results;
 }
 
 getPopularMoviesPreview();
--- src/secret.js
+++ src/secret.js
@@ -1,3 +1,1 @@
 const API_KEY = '759afe2f973e62f0426265a22a2fa385';
-
-export default API_KEY;
